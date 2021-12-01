c5c2d3c78689e76a5d6558a136623b4e64596a3a
Rickard Börjesson (bwf665) <rickard.borjesson@teliacompany.com>
2021-12-01 15:32:17 +0100

    Refactor template

--- src/01/index.ts
+++ src/01/index.ts
@@ -6,13 +6,13 @@ const countIncrements = (data: number[]) =>
     data.reduce((acc: number, curr, i) => acc + (i > 0 && curr > data[i - 1] ? 1 : 0), 0);
 
 const part1 = () => {
-    console.log('Part 1: ', countIncrements(data));
+    return countIncrements(data);
 };
 
 const part2 = () => {
     const windows = data.map((_, i) => data.slice(i, i + 3).reduce((acc, curr) => acc + curr), []);
-    console.log('Part 2: ', countIncrements(windows));
+    return countIncrements(windows);
 };
 
-part1();
-part2();
+console.log('Part 1: ', part1());
+console.log('Part 2: ', part2());
