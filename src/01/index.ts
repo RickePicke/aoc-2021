afaa113db292ce857df644f8fe5a6bd6a0e92d10
Rickard Börjesson (bwf665) <rickard.borjesson@teliacompany.com>
2021-12-05 22:42:15 +0100

    Day 1 fix indent

--- src/01/index.ts
+++ src/01/index.ts
@@ -3,15 +3,15 @@ import { asArray } from '../utils/read-input';
 const data = asArray('src/01/input.txt').map((n) => parseInt(n));
 
 const countIncrements = (data: number[]) =>
-    data.reduce((acc: number, curr, i) => acc + (i > 0 && curr > data[i - 1] ? 1 : 0), 0);
+  data.reduce((acc: number, curr, i) => acc + (i > 0 && curr > data[i - 1] ? 1 : 0), 0);
 
 const part1 = () => {
-    return countIncrements(data);
+  return countIncrements(data);
 };
 
 const part2 = () => {
-    const windows = data.map((_, i) => data.slice(i, i + 3).reduce((acc, curr) => acc + curr), []);
-    return countIncrements(windows);
+  const windows = data.map((_, i) => data.slice(i, i + 3).reduce((acc, curr) => acc + curr), []);
+  return countIncrements(windows);
 };
 
 console.log('Part 1: ', part1());
